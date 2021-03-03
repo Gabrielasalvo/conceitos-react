16b3bbbfe3e868f4c76a0540dc4e6600f7efd849
Gabriela Salvo <gabrielamdesalvo@gmail.com>
2021-03-03 18:53:26 -0300

    searching projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import api from "./services/api";
 
 function App() {
+  const [projects, setProjects] = React.useState([]);
+  React.useEffect(() => {
+    api.get("/projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+  async function handleAddProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    const response = await api.post("/projects", {
+      title: "Frontend",
+      owner: "Gabriela",
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="Homepage" />
+      <header> Conteúdo</header>
+
+      {projects.map((project) => (
+        <li key={project.id}>{project.title}</li>
+      ))}
+      <button type="button" onClick={handleAddProject}>
+        Adicionar Projeto
+      </button>
     </div>
   );
 }
